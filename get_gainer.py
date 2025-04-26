d42c613b40e8741ffe524412e066a3d05953f39e
geotechnick <102661900+geotechnick@users.noreply.github.com>
2025-04-26 16:51:52 -0400

    Update get_gainer.py

--- get_gainer.py
+++ get_gainer.py
@@ -1,53 +1,17 @@
-# get_gainer.py - Main script to execute processing
-main_script = """
+from factory import GainerFactory
 import sys
-from bin.gainers.factory import GainerFactory
-
-class ProcessGainer:
-    def __init__(self, gainer_downloader, gainer_normalizer):
-        self.downloader = gainer_downloader
-        self.normalizer = gainer_normalizer
-    
-    def _download(self):
-        self.downloader.download()
-    
-    def _normalize(self):
-        self.normalizer.normalize()
-    
-    def _save_to_file(self):
-        self.normalizer.save_with_timestamp()
-    
-    def process(self):
-        self._download()
-        self._normalize()
-        self._save_to_file()
 
 if __name__ == "__main__":
-    choice = sys.argv[1]
+    # Read command-line argument for the gainer type (e.g., 'yahoo')
+    choice = sys.argv[1]  # e.g., 'yahoo'
+
+    # Create a factory instance based on the user's choice
     factory = GainerFactory(choice)
-    downloader = factory.get_downloader()
-    normalizer = factory.get_processor()
-    runner = ProcessGainer(downloader, normalizer)
-    runner.process()
-"""
 
-# Makefile update
-makefile = """
-gainers:
-	python get_gainer.py $(SRC)
-"""
+    # Get the appropriate gainer instance
+    gainer = factory.get_gainer()
 
-files = {
-    "bin/gainers/base.py": template,
-    "bin/gainers/yahoo.py": yahoo,
-    "bin/gainers/wsj.py": wsj,
-    "bin/gainers/factory.py": factory,
-    "get_gainer.py": main_script,
-    "Makefile": makefile,
-}
-
-for filename, content in files.items():
-    with open(filename, "w") as f:
-        f.write(content)
-
-print("Refactored code has been written to the appropriate files.")
+    # Process the gainer (download, normalize, save)
+    gainer.download()
+    gainer.normalize()
+    gainer.save_with_timestamp()
